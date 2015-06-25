5de27e5b3d97fe8a1ad7dbf7c1b98b893e41f13f
Alex Blair <alexblair190@gmail.com>
2015-06-25 17:33:50 -0600

    checkpoint 43

---
+++ Blocstagram/Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Stephen Blair on 6/25/15.
+//  Copyright (c) 2015 blairgraphix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *) sourceImage;
+
+@end
